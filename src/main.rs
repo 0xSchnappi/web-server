438430e9af6aed62c084f47468f1ff35b19028b5
0xSchnappi <952768182@qq.com>
2024-07-16 17:06:52 +0800

    读取请求

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:80").unwrap();
@@ -7,5 +10,18 @@ fn main() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let http_request: Vec<_> = buf_reader
+        .lines() // Lines方法创建一个迭代器，对内容流按行迭代，要使用Lines，必须先引入std::io::BufRead
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect(); // collect 消费掉迭代器
+
+    println!("Request: {:#?}", http_request);
 }
