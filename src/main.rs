6fea7ac8fb14876441b21494633500a37dc836ac
0xSchnappi <952768182@qq.com>
2024-07-16 17:53:30 +0800

    验证请求和选择性应答

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,21 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
-    let http_request: Vec<_> = buf_reader
-        .lines() // Lines方法创建一个迭代器，对内容流按行迭代，要使用Lines，必须先引入std::io::BufRead
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect(); // collect 消费掉迭代器
+    // let http_request: Vec<_> = buf_reader
+    //     .lines() // Lines方法创建一个迭代器，对内容流按行迭代，要使用Lines，必须先引入std::io::BufRead
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect(); // collect 消费掉迭代器
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(file_name).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
